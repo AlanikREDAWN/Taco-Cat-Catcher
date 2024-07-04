95cd5c3f7de6c7a8ea6e8ad2382ca7486054569d
Alanik <152527270+AlanikREDAWN@users.noreply.github.com>
2024-07-04 18:56:11 +0000

    started adding how to play and start screens

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 //Move the catcher with the left and right arrow keys to catch the falling objects. 
 
-new Q5();
-
 /* VARIABLES */
 //sprites
 let catcher, fallingObject, badFallingObject;
 let playButton, directionsButton, backButton;
-//scores
+//ints
 let score = 0;
 let highScore = 0;
+let screen = 0;
 //images
 var backgroundImg;
 var catcherImg;
@@ -22,28 +21,25 @@ let greenColor = '#519053';
 var bgColor = '#4ECDC4';
 let textColor = '#008080';
 let highScoreColor = '#d3eddc';
-createCanvas(400,400, '2d');
-// preload();
 
 /* PRELOAD LOADS FILES */
-// function preload(){
-  // backgroundImg = loadImage('assets/burritoCat.gif');
-  backgroundImg = loadImage('assets/old/burritoCat_old.png');
-  // catcherImg = loadImage("assets/plate.gif");
-  catcherImg = loadImage("assets/old/plate_old.png");
-  // fallingObjectImg1 = loadImage("assets/tacoCat1.gif");
-  fallingObjectImg1 = loadImage("assets/old/tacoCat1_old.png");
-  // fallingObjectImg2 = loadImage("assets/tacoCat2.gif");
-  fallingObjectImg2 = loadImage("assets/old/tacoCat2_old.png");
-  // badFallingObjectImg = loadImage("assets/pugBurger.gif");
-   badFallingObjectImg = loadImage("assets/old/pugBurger_old.png");
-
-// }
+function preload() {
+  backgroundImg = loadImage('assets/burritoCat.gif');
+  // backgroundImg = loadImage('assets/old/burritoCat_old.png');
+  catcherImg = loadImage("assets/plate.gif");
+  // catcherImg = loadImage("assets/old/plate_old.png");
+  fallingObjectImg1 = loadImage("assets/tacoCat1.gif");
+  // fallingObjectImg1 = loadImage("assets/old/tacoCat1_old.png");
+  fallingObjectImg2 = loadImage("assets/tacoCat2.gif");
+  // fallingObjectImg2 = loadImage("assets/old/tacoCat2_old.png");
+  badFallingObjectImg = loadImage("assets/pugBurger.gif");
+   // badFallingObjectImg = loadImage("assets/old/pugBurger_old.png");
+}
 
 /* SETUP RUNS ONCE */
-// function setup() {
+function setup() {
   
-  // createCanvas(400,400);
+  createCanvas(400, 400);
 
   // fallingObjects = [fallingObjectImg1, fallingObjectImg2]; -> TO-DO: figure out how to make image for fallingObject sprite random (50% chance of each image)
   
@@ -70,108 +66,128 @@ createCanvas(400,400, '2d');
   badFallingObject.vel.y = 2;
   badFallingObject.rotationLock = true;
 
-  //navigationButtons
-  // playButton = new Sprite(createCanvas)
-
   //debug
   // allSprites.debug = true;
-
-// }
+  homeScreen();
+}
 
 /* DRAW LOOP REPEATS */
 function draw() {
-   // createCanvas(400,400);
   //set up screen
   background(bgColor);
-  //Draw background image
-  backgroundImg.createImage(230, 5);
-
-  fill(textColor);
-  textSize(12);
-  // Draw directions to screen
-  textAlign('right', 'baseline');
-  text("Move the plate \n with the left and \n right arrow keys \n to catch the taco \n cats and avoid \n the imposters", width-10, 20);
-
-  //If fallingObject reaches bottom, move back to random position at top
-  if (fallingObject.y >= height) {
-    fallingObject.y = 0;
-    fallingObject.x = random(width);
-    fallingObject.vel.y = random(1, 5);
-    score -= 1;
-  }
-  //If badFallingObject reaches bottom, move back to random position at top
-  if (badFallingObject.y >= height) {
-    badFallingObject.y = 0;
-    badFallingObject.x = random(width);
-    badFallingObject.vel.y = random(1, 5);
+  if (screen == 0) {
+    if (directionsButton.mouse.pressed()) {
+      screen = 1;
+      directionsScreen();
+    }
   }
 
-  //Move catcher
-  if (kb.pressing("left")) {
-    catcher.vel.x = -3;
-  } else if (kb.pressing("right")) {
-    catcher.vel.x = 3;
-  } else {
-    catcher.vel.x = 0;
+  if (screen == 1) {
+    if (backButton.mouse.pressed()) {
+      screen = 0;
+      backButton.pos = {x: -900, y: -900};
+      homeScreen();
+    }
   }
 
-  //Stop catcher at edges of screen
-  if (catcher.x < 50) {
-    catcher.x = 50;
-  } else if (catcher.x > 350) {
-    catcher.x = 350;
-  }
+  if (screen == 2) {
 
-  //If fallingObject collides with catcher, move back to random position at top
-  if (fallingObject.collides(catcher)) {
-    fallingObject.y = 0;
-    fallingObject.x = random(width);
-    fallingObject.vel.y = random(1, 5);
-    fallingObject.direction = 'down';
-    score += 1;
-  }
-
-  //If badFallingObject collides with catcher, move back to random position at top
-  if (badFallingObject.collides(catcher)) {
-    badFallingObject.y = 0;
-    badFallingObject.x = random(width);
-    badFallingObject.vel.y = random(1, 5);
-    badFallingObject.direction = 'down';
-    score -= 1;
-  }
-
-  if (fallingObject.collides(badFallingObject)) {
-    fallingObject.direction = 'down';
-    badFallingObject.direction = 'down';
-  }
-
-  //Display score
-  textAlign('left', 'bottom');
-  textStyle('bold');
-  textSize(16);
-  fill(textColor);
-  text('Score: ' + score, 10, 25);
-
-  //Create lose state
-  if (score < 0) {
-    catcher.x = 500;
-    fallingObject.y = -500;
-    badFallingObject.x = -500;
+    //set up screen
     background(bgColor);
-    textSize(30);
-    textAlign('center');
+    
+    //If fallingObject reaches bottom, move back to random position at top
+    if (fallingObject.y >= height) {
+      fallingObject.y = 0;
+      fallingObject.x = random(width);
+      fallingObject.vel.y = random(1, 5);
+      score -= 1;
+    }
+    //If badFallingObject reaches bottom, move back to random position at top
+    if (badFallingObject.y >= height) {
+      badFallingObject.y = 0;
+      badFallingObject.x = random(width);
+      badFallingObject.vel.y = random(1, 5);
+    }
+
+    //Move catcher
+    if (kb.pressing("left")) {
+      catcher.vel.x = -3;
+    } else if (kb.pressing("right")) {
+      catcher.vel.x = 3;
+    } else {
+      catcher.vel.x = 0;
+    }
+
+    //Stop catcher at edges of screen
+    if (catcher.x < 50) {
+      catcher.x = 50;
+    } else if (catcher.x > 350) {
+      catcher.x = 350;
+    }
+    
+    //If fallingObject collides with catcher, move back to random position at top
+    if (fallingObject.collides(catcher)) {
+      fallingObject.y = 0;
+      fallingObject.x = random(width);
+      fallingObject.vel.y = random(1, 5);
+      fallingObject.direction = 'down';
+      score += 1;
+    }
+
+    //If badFallingObject collides with catcher, move back to random position at top
+    if (badFallingObject.collides(catcher)) {
+      badFallingObject.y = 0;
+      badFallingObject.x = random(width);
+      badFallingObject.vel.y = random(1, 5);
+      badFallingObject.direction = 'down';
+      score -= 1;
+    }
+
+    if (fallingObject.collides(badFallingObject)) {
+      fallingObject.direction = 'down';
+      badFallingObject.direction = 'down';
+    }
+
+    //Display score
+    textAlign('left', 'bottom');
+    textStyle('bold');
+    textSize(16);
     fill(textColor);
-    text('You lost!', 200, 200);
+    text('Score: ' + score, 10, 25);
 
-    textSize(15);
-    textStyle('normal');
-    fill(highScoreColor);
-    text('Click anywhere to play again', 200, 220);
-    restart();
+    //Create lose state
+    if (score < 0) {
+      catcher.x = 500;
+      fallingObject.y = -500;
+      badFallingObject.x = -500;
+      background(bgColor);
+      textSize(30);
+      textAlign('center');
+      fill(textColor);
+      text('You lost!', 200, 200);
+
+      textSize(15);
+      textStyle('normal');
+      fill(highScoreColor);
+      text('Click anywhere to play again', 200, 220);
+      restart();
+    }
+
+    youWin();
+
   }
+  // //set up screen
+  // background(bgColor);
+  // //Draw background image
+  // image(backgroundImg, 230, 5);
 
-  youWin();
+  // fill(textColor);
+  // textSize(12);
+  // // Draw directions to screen
+  // textAlign('right', 'baseline');
+  // text("Move the plate \n with the left and \n right arrow keys \n to catch the taco \n cats and avoid \n the imposters", width-10, 20);
 
+ 
   //debug
   // allSprites.debug = mouse.pressing();
 }
@@ -211,4 +227,42 @@ function youWin() {
     text('Click anywhere to play again', 200, 220);
     restart();
   }
+}
+
+function homeScreen() {
+  background(bgColor);
+
+  fill(textColor);
+  textSize(20);
+  textAlign(CENTER);
+  text("Taco Cat Catcher", width/2, height/2, + 100);
+
+  fill(highScoreColor);
+  textSize(12);
+
+  //navigationButtons
+  directionsButton = new Sprite(width/2 - 100, height/2 + 50, 100, 70, 'k');
+  directionsButton.color = textColor;
+  directionsButton.text = "How to Play";
+  playButton = new Sprite(width/2 + 100, height/2 + 50, 100, 70, 'k');
+  playButton.color = textColor;
+  playButton.text = "Play";
+}
+
+function directionsScreen() {
+  backButton = new Sprite(width/2, height/2, 100, 70, 'k');
+  backButton.color = textColor;
+  backButton.text = "backButton";
+  playButton.pos = {x: -600, y: -600};
+  directionsButton.pos = {x: -800, y: -800};
+  background(bgColor);
+  fill(textColor);
+  textSize(12);
+  // Draw directions to screen
+  textAlign('center');
+  text("Move the plate \n with the left and \n right arrow keys \n to catch the taco \n cats and avoid \n the imposters", width/2, height/2);
+}
+
+function playScreenAssets() {
+
 }
