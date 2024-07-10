1c66a41fe3d0d9f5903386f707fd5c6b146d864e
Alanik <152527270+AlanikREDAWN@users.noreply.github.com>
2024-07-10 21:47:59 +0000

    Still implementing supabase

--- replit.nix
+++ replit.nix
@@ -1,5 +1,11 @@
-{ pkgs }: {
+{ pkgs ? import <nixpkgs> {} }: {
   deps = [
+    pkgs.nodejs-14_x
+    pkgs.nodePackages.typescript
+    pkgs.nodePackages.webpack
+    pkgs.nodePackages.webpack-cli
+    pkgs.nodePackages.ts-loader
+    pkgs.nodePackages.copyfiles
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
   ];
